78c4adbdabfb2c5117796fbadb88f50818c2146a
Loan Duyên <43545283+duyenlan151@users.noreply.github.com>
2021-05-14 15:25:55 +0700

    #update validate form

--- components/blog/detail.tsx
+++ components/blog/detail.tsx
@@ -1,21 +1,23 @@
 import React, { useEffect, useState, useMemo } from "react";
 import { useForm, Controller } from "react-hook-form";
-// import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from "Yup";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
 // import Select from "react-select";
 
 const schema = yup.object().shape({
-    // firstName: yup.string().required(),
-    // age: yup.number().positive().integer().required(),
-    title: yup.string().required(),
-  });
+  author: yup.string().required(),
+  title: yup.string().required(),
+  content: yup.string().required(),
+  description: yup.string().required(),
+  publishedAt: yup.date().required(),
+});
 
 interface IFormInput{
   author: string,
   title: string,
   content: string,
   description: string,
-  publishedAt: string,
+  publishedAt: Date,
 }
 
 function Detail(props){
@@ -23,36 +25,17 @@ function Detail(props){
     const [data, setData] = useState({});
     const [myBlog, setMyBlog] = useState({});
     const [isEdit, setIsEdit] = useState(true);
-    // const formOptions = { resolver: yupResolver(schema) };
-    // const { register, handleSubmit, watch, formState: { errors } } = useForm(formOptions);
-     
-    // const { register, handleSubmit, formState:{ errors } } = useForm({
-    //     resolver: yupResolver(schema)
-    // });
-    // const onSubmit = data => console.log(data);
 
     useEffect(() => {   
         let dataLocal = JSON.parse(localStorage.getItem('blogs'));
-        // setMyBlog(dataLocal.find((elm) => elm.url === id));
-        // console.log('register: ', validationSchema);   
-        // let dataLocal = JSON.parse(localStorage.getItem('blogs'));
-        let blog = dataLocal.find((elm) => elm.url === id);
+        let blog = dataLocal.find((elm) => elm.url = id);
         setMyBlog(blog);
-        // const { password, confirmPassword, ...defaultValues } = blog;
-        // formOptions.defaultValues = defaultValues;
-        // console.log(formOptions);
-        // console.log(register);
     }, [])
 
     // Submit lưu data
     const handleSubmitForm = (e) => {
         e.preventDefault();
         setIsEdit(true);
-        // let id = myBlog[`${url}`]; 
-        // let index =  data.findIndex((elm) => elm.url === id);
-        // data[index] = myBlog;
-        // localStorage.setItem("blogs", JSON.stringify(data));
-        // router.push('/blogs');
     }
 
     // handle click butotn edit
@@ -61,18 +44,27 @@ function Detail(props){
         reset();
     }
 
+    // Listen myBLog if data change
     useEffect(() => {
       reset(myBlog)
     }, [myBlog]);
 
-    const { register, reset, handleSubmit } =useForm<IFormInput>({
+    // React hook  form config
+    const { register, reset, handleSubmit, formState: { errors } } = useForm<IFormInput>({
       defaultValues: myBlog,
+      resolver: yupResolver(schema)
     });
     
+    // handle on submit data when edit form
     const onSubmit = (data, e) => {
       e.preventDefault();
-      console.log(data);
 
+      let localData =  JSON.parse(localStorage.getItem("blogs"));
+      let index = localData.findIndex((elm) => elm['url'] === id);
+      localData[index] = data;
+      
+      localStorage.setItem("blogs", JSON.stringify(localData));
+      setMyBlog(data);
       setIsEdit(true);
       reset();
     };
@@ -103,6 +95,7 @@ function Detail(props){
                   {...register("author")} 
                   disabled={isEdit}
                 />
+                {errors.author?.message && <div className="mt-1 text-danger">{errors.author?.message}</div>}
               </div>
               <div className="form-group">
                 <label htmlFor="pwd">Title:</label>
@@ -113,6 +106,7 @@ function Detail(props){
                   {...register("title")} 
                   disabled={isEdit}
                 />
+                {errors.title?.message && <div className="mt-1 text-danger">{errors.title?.message}</div>}
               </div>
               <div className="form-group">
                 <label htmlFor="pwd">Images:</label>
@@ -133,6 +127,7 @@ function Detail(props){
                   {...register("content")} 
                   disabled={isEdit}
                 />
+                {errors.content?.message && <div className="mt-1 text-danger">{errors.content?.message}</div>}
               </div>
               <div className="form-group">
                 <label htmlFor="pwd">Description:</label>
@@ -143,6 +138,7 @@ function Detail(props){
                   {...register("description")} 
                   disabled={isEdit}
                 />
+                {errors.description?.message && <div className="mt-1 text-danger">{errors.description?.message}</div>}
               </div>
               <div className="form-group">
                 <label htmlFor="pwd">Date Create:</label>
